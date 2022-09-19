0d3597ca2911f74df0a0dba85330153113d75ddc
Lee <lsmin476@naver.com>
2022-09-19 18:45:17 +0900

    java subject 1-2 0919 final text

--- chap2-prob2/src/MonthName.java
+++ chap2-prob2/src/MonthName.java
@@ -4,14 +4,14 @@ public class MonthName {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String month;
-		int integer_Month = 0;
+		String month;                 // 월의 이름 선언
+		int integer_Month = 0;        // 정수로 받을 월을 선언 및 0으로 초기화
 
 		System.out.printf("월의 이름을 입력하시오:");
-		month = sc.next();
+		month = sc.next();            // 정수 읽기
 
-		switch (month) {
-		case "January":
+		switch (month) {              // switch case 구문을 사용하여 입력받은 월에 따라서
+		case "January":				  // 월을 정수로 새롭게 초기화
 			integer_Month = 1;
 			break;
 		case "February":
@@ -48,12 +48,11 @@ public class MonthName {
 			integer_Month = 12;
 			break;
 		default:
-			System.out.println("잘못된 월 이름입니다!!");
-			sc.close();
+			System.out.println("잘못된 월 이름입니다!!");     // 입력받은 월이 잘못되었으면 
+			sc.close();								   // 잘못되었다고 출력 후 프로그램 종료
 			return;
 		}
-
-		System.out.println(integer_Month + "월 입니다!!");
+		System.out.println(integer_Month + "월 입니다!!");  // 정수로 저장된 월 출력
 
 		sc.close();
 
